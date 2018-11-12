2afa831c52c229d653d5d65b272b5d56e6b923b7
karenparente <kah.parente@gmail.com>
2018-11-12 11:11:18 -0200

    Add files via upload

---
+++ Entidade.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "Entidade.h"
+
+
+Entidade::Entidade()
+{
+
+}
+
+
+Entidade::~Entidade()
+{
+
+}
+
+void Entidade::Desenha(RenderWindow& window)
+{
+    window.draw(objP);
+}
---
+++ Entidade.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "pch.h"
+class Entidade
+{
+protected:
+    RectangleShape objP;
+    Texture* textura;
+    Vector2f posicao(500.0f, 500.0f); //altera na inicializa
+
+
+public:
+	Entidade();
+	~Entidade();
+	virtual void Inicializa() = 0;
+	void Desenha(RenderWindow& window);
+};
+

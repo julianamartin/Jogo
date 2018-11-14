edbc06ce6e6581ac882262ae1b6d3b6c7a21f7ab
karenparente <kah.parente@gmail.com>
2018-11-14 20:09:41 -0200

    Update Fase.h

--- v_9.0.1/Fase.h
+++ v_9.0.1/Fase.h
@@ -1,18 +1,37 @@
 #pragma once
 #include "Jogador.h"
 #include"Plataforma.h"
+#include "Jogador.h"
+#include "DP.h"
+#include "Prova.h"
+#include "Conta.h"
+#include "Alfajor.h"
+#include "Cerveja.h"
+#include "Bebedouro.h"
+#include "ListaEntidade.h"
+
 class Fase
 {
 private:
 	Texture jog1Texture;
 	Texture jog2Texture;
+
 protected:
 	Jogador jog1;
 	Jogador jog2;
+	int n_dp;
+	int n_prova;
+	int n_conta;
+	int n_alfajor;
+	int n_cerveja;
+	int n_bebed;
+
 	RenderWindow* window;
 	float deltaTime;
 	bool multiplayer;
-	vector <Plataforma> plataformas;
+	Plataforma plat;
+	void verificaColisao();
+	ListaEntidade listaEnt;
 
 public:
 	Fase();
@@ -20,6 +39,7 @@ public:
 	void Inicializa(RenderWindow* window, float deltaTime, bool multiplayer);
 	virtual void Executar() = 0;
 	RenderWindow* getWindow() { return window; }
+	virtual void criaElem() = 0;
 
 };
 

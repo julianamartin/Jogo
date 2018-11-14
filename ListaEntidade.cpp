8c68d57d6ddabbc0b202303dafca809f8fa7744a
karenparente <kah.parente@gmail.com>
2018-11-14 20:03:41 -0200

    Add files via upload

---
+++ ListaEntidade.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "ListaEntidade.h"
+
+
+ListaEntidade::ListaEntidade()
+{
+}
+
+
+ListaEntidade::~ListaEntidade()
+{
+}
+
+void ListaEntidade::Desenhalista()
+{
+}
+
+void ListaEntidade::InicializaLista()
+{
+}

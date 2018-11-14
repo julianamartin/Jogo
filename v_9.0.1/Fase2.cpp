c006b388334e7ee869e2bfce628d0d1b5d543f79
karenparente <kah.parente@gmail.com>
2018-11-14 20:08:42 -0200

    Update Fase2.cpp

--- v_9.0.1/Fase2.cpp
+++ v_9.0.1/Fase2.cpp
@@ -2,40 +2,91 @@
 #include "Fase2.h"
 
 
-Fase2::Fase2():
-Fase()
+Fase2::Fase2() :
+	Fase()
 {
-	this->window = window;
+	n_prova = (rand() % 5) + 5;
+	n_dp = (rand() % 5) + 5;
+	n_bebed = (rand() % 5) + 5;
+	n_cerveja = (rand() % 5) + 5;
+	plat.Inicializa2();
+	criaElem();
 }
 Fase2::~Fase2()
 {
 }
 
-void Fase2::DesenhaElem()
+void Fase2::Executar()
 {
-	vector<Plataforma> plataformas;
-	plataformas.push_back(Plataforma(nullptr, Vector2f(400.0f, 200.0f), Vector2f(500.0f, 200.0f)));
-	plataformas.push_back(Plataforma(nullptr, Vector2f(400.0f, 200.0f), Vector2f(500.0f, 0.0f)));
-	plataformas.push_back(Plataforma(nullptr, Vector2f(1000.0f, 200.0f), Vector2f(500.0f, 500.0f)));
+	float deltaTime = 0.0f;
+	Clock clock;
+
+	jog1.setPosicao2();
+	if (multiplayer)
+		jog2.setPosicao2();
 
 	while (window->isOpen())
 	{
-		Vector2f direção;
-		for (Plataforma& plataforma : plataformas) //DEPOIS DO ATUALIZAAAA
+		deltaTime = clock.restart().asSeconds();
+		if (deltaTime > 1.0f / 20.0f)
+			deltaTime = 1.0f / 20.0f;
+
+		Event evnt;
+		while (window->pollEvent(evnt))
 		{
-			if (plataforma.getColisão().Colidiu(jog1.getColisão(), direção, 1.0f))
+			switch (evnt.type)
 			{
-				jog1.EmColisão(direção);
+			case Event::Closed:
+				window->close();
+				break;
 			}
 		}
-		for (Plataforma& plataforma : plataformas)
+
+		jog1.Atualiza1(deltaTime);
+		verificaColisao();
+
+		window->clear(Color(150, 150, 150));
+		jog1.Desenha(*window);
+		if (multiplayer)
 		{
-			plataforma.Desenha(*window);
+			jog2.Atualiza2(deltaTime);
+			jog2.Desenha(*window);
 		}
+		plat.Desenha(*window);
+		window->display();
+
 	}
 }
 
-void Fase2::Executar()
+void Fase2::criaElem()
 {
+	int i;
+	DP* pD = NULL;
+	for (i = 0; i < n_dp; i++)
+	{
+		pD = new DP; //classe nao está bem definida
+		listaEnt.Incluir(static_cast<Entidade*> (pD));
+	}
 
+	Prova* pP = NULL;
+	for (i = 0; i < n_prova; i++)
+	{
+		pP = new Prova;
+		listaEnt.Incluir(static_cast<Entidade*> (pP));
+	}
+
+	Bebedouro* pBe = NULL;
+	for (i = 0; i < n_bebed; i++)
+	{
+		pBe = new Bebedouro;
+		listaEnt.Incluir(static_cast<Entidade*> (pBe));
+	}
+
+	Cerveja* pCe = NULL;
+	for (i = 0; i < n_cerveja; i++)
+	{
+		pCe = new Cerveja;
+		listaEnt.Incluir(static_cast<Entidade*> (pCe));
+	}
 }
+
